cbcc18be64f6e80dd747c120086d488fe2f05f6c
Marwane265 <Marwane6.35@hotmail.com>
2023-03-08 11:25:13 +0100

    init commit

---
+++ src/be/intecbrussel/FirstApp.java
@@ -0,0 +1,14 @@
+package be.intecbrussel;
+
+public class FirstApp {
+
+    //class name -> PascalCase
+    //variable/ method -> camelCase
+
+    public static void main(String [] args){
+
+        System.out.println("Hello world");
+
+    }
+
+}
